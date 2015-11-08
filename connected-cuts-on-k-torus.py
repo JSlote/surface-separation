311deaccb63a395cc717533ba44a3b8a77c35ff3
JSlote <joseph.slote@gmail.com>
2015-11-08 14:13:58 -0600

    add algorithms

--- connected-cuts-on-k-torus.py
+++ connected-cuts-on-k-torus.py
@@ -11,7 +11,8 @@ Initialize it with a dictionary of nodes:edgeCycle pairs
 
 E.g., one RotationSystem for the eye graph would be:
 
-rotSysList = {1:(1,2,3,4), 2:(2,3,1,4)}
+rotSysList = {1:[1,2,3,4], 2:[2,3,1,4]}
+
 myRotSys = RotationSystem(rotSysList)
 
 Then you can do:
@@ -22,6 +23,8 @@ myRotSys.nextUndirEdge(node,currentEdge)
 myRotSys.nextDirEdge(node,currentEdge)
 myRotSys.eulerChar
 
+TODO: currently can't handle loops:
+The bouquet, {1:(1,3,2,1,3,2)}, won't work (I don't think)
 """
 class RotationSystem(object):
 	def __init__(self, rotSysList):
@@ -50,10 +53,78 @@ class RotationSystem(object):
 		nextEdgeIndex = (currentEdgeIndex+1) % len(theCycle)
 		return theCycle[nextEdgeIndex]
 
-	def nextDirEdge(self, node, edge):
-		nextUndirEdge = self.nextEdge(node, edge)
+	def nextDirEdge(self, edge):
+		inboundNode = self.dirEdges[edge][1]
+		nextUndirEdge = self.nextUndirEdge(inboundNode, edge[0])
 		#return the correct dir edge (has the right "to" node)
-		if self.dirEdges[(nextUndirEdge,True)][0] is node:
-			return (nextUndirEdge,True)
+		if self.dirEdges[(nextUndirEdge,True)][0] is inboundNode:
+			return (nextUndirEdge, True)
 		else:
 			return (nextUndirEdge, False)
+
+
+def countDirectedCycles(TG):
+	waitingEdges = TG.dirEdges.keys()
+	cycles = []
+	while waitingEdges: #is not empty
+		currentEdge = waitingEdges[0]
+		thisCycle = []
+		while currentEdge not in thisCycle:
+			waitingEdges.remove(currentEdge)
+			thisCycle.append(currentEdge)
+			currentEdge = TG.nextDirEdge(currentEdge)
+		cycles.append(thisCycle)
+	return len(cycles)
+
+def isMinimal(TG):
+	waitingNodes = []
+
+	# Pick an arbitrary edge in TG
+	firstEdge = next(TG.undirEdges.iterkeys())
+
+	# Assign it a direction arbitrarily
+
+	# edgeDirDict holds assigned directions
+	# key is edge, value is "to" node
+	edgeDirDict = { firstEdge : TG.undirEdges[firstEdge][1]}
+
+	# Add the two nodes incident to that first edge to waitingNodes
+	waitingNodes = list(TG.undirEdges[firstEdge])
+
+	while waitingNodes: # while waitingNodes has nodes in it
+		# Pop vertex V from Waiting
+		currNode = waitingNodes.pop()
+		# Find an edge incident to currNode that has a direction already assigned:
+		currNodeEdgeList = TG.nodes[currNode]
+		for i in xrange(len(currNodeEdgeList)):
+			if currNodeEdgeList[i] in edgeDirDict:
+				startEdgeIndex = i
+				break
+
+		# Try to assign directions alternating between inward and outward along the cyclic ordering.
+		# create a variable to toggle 'away' and 'to'
+		shouldBeToCurrNode = edgeDirDict[currNodeEdgeList[startEdgeIndex]] is not currNode
+		# iterate through the other edges
+		for i in xrange(len(currNodeEdgeList)-1):
+			index = (startEdgeIndex + i + 1) % len(currNodeEdgeList)
+			edge = currNodeEdgeList[index]
+			if edge in edgeDirDict:
+				# if the current edge in question already had an incompatible direction assigned
+				if (edgeDirDict[edge] == currNode) is not shouldBeToCurrNode:
+					return False
+				#else, it's already correct
+			else: #the current edge doesn't have a direction assigned yet
+				otherNodeOfEdge = TG.undirEdges[edge][1] if (TG.undirEdges[edge][0] == currNode) else TG.undirEdges[edge][0]
+				# Add vertices incident to each edge that is newly assigned a direction to Waiting
+				waitingNodes.append(otherNodeOfEdge)
+				edgeDirDict[edge] = currNode if shouldBeToCurrNode else otherNodeOfEdge
+
+			shouldBeToCurrNode = not shouldBeToCurrNode
+
+	return True #(we had no problems)
+
+rotSysList = {1:[1,2,3,4], 2:[1,4,3,2]}
+myRotSys = RotationSystem(rotSysList)
+print countDirectedCycles(myRotSys)
+print isMinimal(myRotSys)
+# import pdb; pdb.set_trace()
